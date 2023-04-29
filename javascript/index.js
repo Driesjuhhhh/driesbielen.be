bb2542db044c53f54c2b7fbc5bdc9a78b4c5848a
DriesBielenPXL <116563561+DriesBielenPXL@users.noreply.github.com>
2023-04-29 13:02:54 +0200

    Created age function

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function calculateAge() {
+    const birthDate = new Date("September 1, 2004");
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+
+    if (
+        monthDiff < 0 ||
+        (monthDiff === 0 && today.getDate() < birthDate.getDate())
+    ) {
+        age--;
+    }
+
+    return age;
+}
+
+window.onload = function() {
+    const ageParagraph = document.getElementById("age");
+    ageParagraph.textContent = `${calculateAge()} jaar oud.`;
+}
+
+console.log(calculateAge());

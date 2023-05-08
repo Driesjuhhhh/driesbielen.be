51b906fbcdfe644e84122e0749e67133a5e2c50b
DriesBielenPXL <116563561+DriesBielenPXL@users.noreply.github.com>
2023-05-08 22:36:47 +0200

    changed somethings

--- javascript/rr.js
+++ javascript/rr.js
@@ -1,7 +1,7 @@
-window.onload = function() {
-    setTimeout(function() {
-        var videoId = "dQw4w9WgXcQ"; // Replace with the ID of your YouTube video
-        var videoContainer = document.getElementById("video-container");
-        videoContainer.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId + '?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
-        }, 100); // Set the timeout to 5000 milliseconds (5 seconds)
-};
+function openYoutubeVideoOnLoad() {
+
+    // Open the video in a new window
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+
+window.onload = openYoutubeVideoOnLoad;

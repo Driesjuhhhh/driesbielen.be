1b9a84d37b3e4d6ea37ac021d929a5005a97ef01
DriesBielenPXL <116563561+DriesBielenPXL@users.noreply.github.com>
2023-05-12 20:02:38 +0200

    Changed BACKEND_URL

--- PROJECTS/connect4/Frontend/js/config.js
+++ PROJECTS/connect4/Frontend/js/config.js
@@ -1,4 +1,4 @@
 // export const BACKEND_URL = "https://localhost:5001/api/";
-export const BACKEND_URL = "https://v7zl3h3m-5001.euw.devtunnels.ms/api/";
+export const BACKEND_URL = "https://z1875282-5001.euw.devtunnels.ms/api/";
 export const GUID_EMPTY = '00000000-0000-0000-0000-000000000000';
 //https://localhost:5001/api/

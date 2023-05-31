cf3b953253a36a00d79ec8149e6161c87e1eaa27
Driesjuhhhh <dries.bielen@student.pxl.be>
2023-06-01 01:21:36 +0200

    changed default backend url

--- PROJECTS/connect4/Frontend/js/config.js
+++ PROJECTS/connect4/Frontend/js/config.js
@@ -5,7 +5,7 @@ export const GUID_EMPTY = "00000000-0000-0000-0000-000000000000";
 
 let url;
 if (localStorage.getItem("backendUrl") === null || localStorage.getItem("backendUrl").length == 0){
-    url = "https://localhost:5001/api/"
+    url = "https://z1875282-5001.euw.devtunnels.ms/api/"
 } else {
     if (localStorage.getItem("backendUrl").endsWith("/api/")){
         url = localStorage.getItem("backendUrl")

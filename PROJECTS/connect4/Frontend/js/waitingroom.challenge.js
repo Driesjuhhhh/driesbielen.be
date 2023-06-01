4f5cb1e807fdf25f057e47625422aa28f2a2c595
Driesjuhhhh <dries.bielen@student.pxl.be>
2023-06-01 20:28:55 +0200

    changes connect 4

--- PROJECTS/connect4/Frontend/js/waitingroom.challenge.js
+++ PROJECTS/connect4/Frontend/js/waitingroom.challenge.js
@@ -2,7 +2,7 @@ function InvitePlayer(username, userId) {
     // Get the user and message from the input fields
     const user = JSON.parse(localStorage.getItem("user")).user;
 
-    alert(
+    console.log(
         `You (${user.nickName}) invited ${username} (${userId}) to play a game!`
     );
 }

aad804dcf4d90e6ee72f27a985ec321a1e554c5e
Driesjuhhhh <101977453+Driesjuhhhh@users.noreply.github.com>
2023-06-16 14:39:55 +0200

    Added social

---
+++ javascript/socials/snapchat.js
@@ -0,0 +1,7 @@
+function openYoutubeVideoOnLoad() {
+
+    // Open the video in a new window
+    window.location.href = "https://www.snapchat.com/add/driesjuhhhhh?share_id=3gN3t7HkVEM&locale=nl-BE";
+}
+
+window.onload = openYoutubeVideoOnLoad;

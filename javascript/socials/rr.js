9bd10476807e36f72b067f060d98cb00123039ac
Driesjuhhhh <101977453+Driesjuhhhh@users.noreply.github.com>
2023-06-19 00:59:48 +0200

    Redesign of homepage

--- javascript/socials/rr.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", async () => {
-    openSiteOnLoad();
-})
-
-function openSiteOnLoad() {
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-}

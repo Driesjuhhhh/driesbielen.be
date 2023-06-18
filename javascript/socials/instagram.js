ea1ee183ef4756f4962b913c5f75f811c4153b1a
Driesjuhhhh <101977453+Driesjuhhhh@users.noreply.github.com>
2023-06-19 00:22:40 +0200

    Removed irrelevant social media

--- javascript/socials/instagram.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", async () => {
-    openSiteOnLoad();
-})
-
-function openSiteOnLoad() {
-window.location.href = "https://www.instagram.com/driesjuhhhh/";
-}

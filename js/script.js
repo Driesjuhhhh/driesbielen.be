876655ac597a87ba820b6e431c56e72942797812
Driesjuhhhh <dries.bielen10@gmail.com>
2023-10-12 19:37:19 +0200

    created age script

--- js/script.js
+++ js/script.js
@@ -1,90 +1,17 @@
-particlesJS("particles", {
-    particles: {
-        number: {
-            value: 100,
-            density: {
-                enable: true,
-                value_area: 800
-            }
-        },
-        color: {
-            value: "#ffffff"
-        },
-        shape: {
-            type: "circle",
-            stroke: {
-                width: 0,
-                color: "#000000"
-            }
-        },
-        opacity: {
-            value: 0.8,
-            random: true,
-            animation: {
-                enable: true,
-                speed: 1,
-                opacity_min: 0,
-                sync: false
-            }
-        },
-        size: {
-            value: 3,
-            random: true
-        },
-        line_linked: {
-            enable: true,
-            distance: 150,
-            color: "#ffffff",
-            opacity: 0.4,
-            width: 1
-        },
-        move: {
-            enable: true,
-            speed: 2,
-            direction: "none",
-            random: true,
-            straight: false,
-            out_mode: "out",
-            bounce: false,
-        }
-    },
-    interactivity: {
-        detectsOn: "canvas",
-        events: {
-            onHover: {
-                enable: true,
-                mode: "push"
-            },
-            onClick: {
-                enable: true,
-                mode: "push"
-            },
-            resize: true
-        },
-        modes: {
-            repulse: {
-                distance: 100,
-                duration: 0.4
-            },
-            push: {
-                particles_nb: 4
-            }
-        }
-    },
-    retina_detect: true
-});
+document.addEventListener("DOMContentLoaded", async () => {
+    const age = document.getElementById("ageSpan");
+    age.textContent = `${calculateAge()} years old.`;
+})
 
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-    });
-}
+function calculateAge(){
+    const birthDate = new Date("September 1, 2004");
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
 
-window.addEventListener('scroll', function () {
-    var scrollTopButton = document.querySelector('.scroll-top');
-    if (this.window.pageYOffset > 200) {
-        scrollTopButton.style.display = 'block';
-    } else {
-        scrollTopButton.style.display = 'none';
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate)){
+        age--;
     }
-});
+
+    return age;
+}

8f25533c4db0a3ea84df30172a1bf421df7eda61
Driesjuhhhh <dries.bielen10@gmail.com>
2023-10-16 17:05:48 +0200

    Code cleanup

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", async () => {
-console.log("Welcome in console land");
-console.log("This is a custom made website based on code of ASMR Prog");
-console.log("I added some stuff and removed some stuff");
-console.log("Have fun in inspector land");
-})
+    console.log("Welcome in console land");
+    console.log("This is a custom made website based on code of ASMR Prog");
+    console.log("I added some stuff and removed some stuff");
+    console.log("Have fun in inspector land");
+});
 
 function scrollToTop() {
     window.scrollTo({
@@ -11,20 +11,20 @@ function scrollToTop() {
     });
 }
 
-window.addEventListener('scroll', function () {
-    var scrollTopButton = document.querySelector('.scroll-top');
+window.addEventListener("scroll", function () {
+    var scrollTopButton = document.querySelector(".scroll-top");
     if (this.window.pageYOffset > 200) {
-        scrollTopButton.style.display = 'block';
+        scrollTopButton.style.display = "block";
     } else {
-        scrollTopButton.style.display = 'none';
+        scrollTopButton.style.display = "none";
     }
 });
 
-window.addEventListener('scroll', function () {
-    var rightTopIcon = document.querySelector('.right-top-icon');
+window.addEventListener("scroll", function () {
+    var rightTopIcon = document.querySelector(".right-top-icon");
     if (this.window.pageYOffset > 700) {
-        rightTopIcon.style.display = 'none';
+        rightTopIcon.style.display = "none";
     } else {
-        rightTopIcon.style.display = 'block';
+        rightTopIcon.style.display = "block";
     }
 });

94f2e5766fd55199ffa9e3ddbede4141bf53e386
Driesjuhhhh <dries.bielen10@gmail.com>
2024-01-21 16:01:16 +0100

    Added aos

--- js/main.js
+++ js/main.js
@@ -65,6 +65,15 @@
     })
   }
 
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
   /**
    * Back to top button
    */

0eea4a8ac65e1a8a623d9f701bd07d89ccfb798f
Driesjuhhhh <dries.bielen10@gmail.com>
2024-02-23 16:00:11 +0100

    disabled right click

--- PROJECTS/JKA/assets/js/main.js
+++ PROJECTS/JKA/assets/js/main.js
@@ -1,4 +1,4 @@
-// document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault());
 
 (function() {
   "use strict";

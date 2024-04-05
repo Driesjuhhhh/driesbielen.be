7b1e3a9d38c0041d92f8916cee9a6c1f58a981f5
Driesjuhhhh <dries.bielen10@gmail.com>
2024-04-05 16:44:58 +0200

    JKA - V1

--- PROJECTS/JKA/assets/js/redirects/facebook.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", async () => {
-    openSiteOnLoad();
-});
-
-function openSiteOnLoad() {
-    window.location.href = "https://www.facebook.com/events/347596124935904";
-}

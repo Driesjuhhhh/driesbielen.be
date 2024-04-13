935583c37b8f8fcba4d7b36e189c2c6d27081d5e
Driesjuhhhh <dries.bielen10@gmail.com>
2024-04-13 21:02:42 +0200

    JKA v2

---
+++ PROJECTS/JKA/V2/assets/js/redirects/tiktok.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    openSiteOnLoad();
+});
+
+function openSiteOnLoad() {
+    window.location.href = "https://www.tiktok.com/@barbiepoppen.expo";
+}

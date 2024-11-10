2b3e35f4f118c8deb59e59e5cddf6220bc66bdf8
Driesjuhhhh <dries.bielen10@gmail.com>
2024-11-10 15:07:18 +0100

    Pingping web fix

--- PROJECTS/PINGPINGDING/flutter_bootstrap.js
+++ PROJECTS/PINGPINGDING/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1521814649"
+    serviceWorkerVersion: "2950565424"
   }
 });

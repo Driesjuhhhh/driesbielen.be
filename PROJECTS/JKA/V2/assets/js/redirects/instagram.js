49bde466b4594027ab37f11583c140ba72cce29f
Dries Bielen <101977453+Driesjuhhhh@users.noreply.github.com>
2026-02-01 14:48:46 +0100

    Updates

---
+++ PROJECTS/JKA/V2/assets/js/redirects/instagram.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    openSiteOnLoad();
+});
+
+function openSiteOnLoad() {
+    window.location.href = "https://www.instagram.com/barbiepoppen_expo/";
+}

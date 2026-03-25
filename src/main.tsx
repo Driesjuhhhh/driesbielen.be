8ae5502e2747482f85b9e6c229bf633dd619d61e
Dries Bielen <dries.bielen10@gmail.com>
2026-03-25 10:39:08 +0100

    plausible

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 
-  import { createRoot } from 'react-dom/client'
-  import React, { useEffect } from 'react'
-  import App from './app/App.tsx'
-  import './styles/index.css'
-  import AOS from 'aos'
-  import 'aos/dist/aos.css'
+import { createRoot } from 'react-dom/client'
+import React, { useEffect } from 'react'
+import { init } from '@plausible-analytics/tracker'
+import App from './app/App.tsx'
+import './styles/index.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+init({
+  domain: 'driesbielen.be',
+  endpoint: 'https://plausible.driesbielen.be/api/event',
+  hashBasedRouting: true,
+})
 
   function Main() {
     useEffect(() => {

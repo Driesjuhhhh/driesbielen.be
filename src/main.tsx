7f6806268900f858456355aeb19b1f009cc8011c
Dries Bielen <dries.bielen10@gmail.com>
2026-07-08 22:50:30 +0200

    plausible fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 
 import { createRoot } from 'react-dom/client'
 import React, { useEffect } from 'react'
-import { init } from '@plausible-analytics/tracker'
 import App from './app/App.tsx'
 import './styles/index.css'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-
-init({
-  domain: 'driesbielen.be',
-  endpoint: 'https://plausible.driesbielen.be/api/event',
-})
 
   function Main() {
     useEffect(() => {
